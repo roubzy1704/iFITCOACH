077882e8e6b45caf3fd6ea7971b0af1096680d0e
roubzy1704 <roubzy1704@yahoo.com>
2020-05-29 02:00:12 -0500

    Updates

---
+++ precache-manifest.e44d454e458cbc271a76f9a90569dd62.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29dbf3439e7ded31a68e03d578b1889c",
+    "url": "/iFITCOACH/index.html"
+  },
+  {
+    "revision": "9cf309f3099fb30c662a",
+    "url": "/iFITCOACH/static/css/main.7d334f4f.chunk.css"
+  },
+  {
+    "revision": "e07e94c812252de001ac",
+    "url": "/iFITCOACH/static/js/2.a5e7fee5.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/iFITCOACH/static/js/2.a5e7fee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf309f3099fb30c662a",
+    "url": "/iFITCOACH/static/js/main.ee2fc2b2.chunk.js"
+  },
+  {
+    "revision": "f64ec508127f741b19b9",
+    "url": "/iFITCOACH/static/js/runtime-main.c6df23ed.js"
+  },
+  {
+    "revision": "dadde1924d8f7e0e381892e63891af2f",
+    "url": "/iFITCOACH/static/media/HeroImage.0844b1ff.png"
+  },
+  {
+    "revision": "2c491870e6fb0c787c093890b3c0865d",
+    "url": "/iFITCOACH/static/media/bike.f87ed462.png"
+  },
+  {
+    "revision": "e745362cc47f33cf8e6b6dd36adcd1a7",
+    "url": "/iFITCOACH/static/media/elliptical.79a0586f.png"
+  },
+  {
+    "revision": "0dd9e1c657abda15dd1ae066afeb30a0",
+    "url": "/iFITCOACH/static/media/facebook.0dd9e1c6.svg"
+  },
+  {
+    "revision": "01212731becb890c76b410f0e0676991",
+    "url": "/iFITCOACH/static/media/img1.9a620867.png"
+  },
+  {
+    "revision": "5430c4d3a9b7d2706a05d33a10f5accc",
+    "url": "/iFITCOACH/static/media/img2.e93bd275.png"
+  },
+  {
+    "revision": "ba142e220870253e0286be201149a62b",
+    "url": "/iFITCOACH/static/media/img3.00e78d48.png"
+  },
+  {
+    "revision": "b63207537c72852b444e1861b204c9e1",
+    "url": "/iFITCOACH/static/media/img4.91f34bbe.png"
+  },
+  {
+    "revision": "bcafaa4146e7b6c59fe41f1680ab7d65",
+    "url": "/iFITCOACH/static/media/img5.066bafd3.png"
+  },
+  {
+    "revision": "bb8cf19a23c1908313162503c1a361e9",
+    "url": "/iFITCOACH/static/media/img6.9d4ccfed.png"
+  },
+  {
+    "revision": "2558ae7d10b5b748ae7313bcc3703fbb",
+    "url": "/iFITCOACH/static/media/img7.0c175e85.png"
+  },
+  {
+    "revision": "e947d612b6bca7af13df28cd450372e7",
+    "url": "/iFITCOACH/static/media/img8.993b0f4f.png"
+  },
+  {
+    "revision": "19e9872f64dac8b0f6ba08667f30ff2d",
+    "url": "/iFITCOACH/static/media/strength.d84f9ae2.png"
+  },
+  {
+    "revision": "dd99acb463568bedba947c79fdeed2cc",
+    "url": "/iFITCOACH/static/media/treadmill.e1a51c79.png"
+  }
+]);
